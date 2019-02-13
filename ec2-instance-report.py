95277f4786e31588f8ac46bad82c3b098273dfda
Keith Patton <kpatton@tradevela.com>
2019-02-13 15:23:12 +0000

    Added some temporary arguments to get around AWS API_Filter being strictly case-sensitive

--- ec2-instance-report.py
+++ ec2-instance-report.py
@@ -4,25 +4,34 @@ import os
 import argparse
 
 # Make the sript user-friendly by providing some arguments and help options
+# Search filters
 parser = argparse.ArgumentParser(description="Retrieve a list of AWS EC2 instances.")
 parser.add_argument("-c", "--lifecycle", action="store_true", help="All instances matching LIFECYCLE.")
 parser.add_argument("-e", "--elastic-ip", type=str, help="All instances matching the ELASTIC_IP.")
 parser.add_argument("-f", "--private-ip", type=str, help="All instances matching the PRIVATE_IP. ALWAYS DISPLAYED.")
 parser.add_argument("-i", "--id", type=str, help="All instances matching ID, entered as a comma separated list. ALWAYS DISPLAYED.")
-parser.add_argument("-l", "--launchtime", help="Display the instance launch time.", action="store_true")
-parser.add_argument("-n", "--name", type=str, help="(Loose) All instances where 'Name' tag contains NAME, accepts multiple values.")
-parser.add_argument("-N", "--name-exact", type=str, help="(Strict) All instances where 'Name' tag matches NAME exactly, accepts multiple values.")
+#TODO : parser.add_argument("-nu", "--nameupper", type=str, help="(Loose) All instances where 'Name' tag contains NAME, accepts multiple values.")
+parser.add_argument("-NL", "--name-exact-lower", type=str, help="(Strict) All instances where 'name' tag matches NAME exactly, accepts multiple values.")
+parser.add_argument("-NU", "--name-exact-upper", type=str, help="(Strict) All instances where 'NAME' tag matches NAME exactly, accepts multiple values.")
+parser.add_argument("-NS", "--name-exact-sentence", type=str, help="(Strict) All instances where 'Name' tag matches NAME exactly, accepts multiple values.")
 parser.add_argument("-x", "--custom-tag", action='append', help="(Loose) All instances where tag is like CUSTOM_TAG, accepts multiple values.")
-parser.add_argument("-o", "--owner", type=str, help="(Loose) All instances where 'Owner' tag contains OWNER, entered as a comma separated list. ALWAYS DISPLAYED.")
-parser.add_argument("-O", "--owner-exact", type=str, help="(Strict) All instances where 'Owner' tag matches OWNER exactly, entered as a comma separated list.")
-parser.add_argument("-p", "--project", type=str, help="(Loose) All instances where 'Project' tag contains PROJECT, accpets multiple values. ALWAYS DISPLAYED.")
-parser.add_argument("-P", "--project-exact", type=str, help="(Strict) All instances where 'Project' tag matches PROJECT exactly, accepts multiple values.")
+#TODO : parser.add_argument("-o", "--owner", type=str, help="(Loose) All instances where 'Owner' tag contains OWNER, entered as a comma separated list. ALWAYS DISPLAYED.")
+parser.add_argument("-OL", "--owner-exact-lower", type=str, help="(Strict) All instances where 'owner' tag matches OWNER exactly, entered as a comma separated list.")
+parser.add_argument("-OU", "--owner-exact-upper", type=str, help="(Strict) All instances where 'OWNER' tag matches OWNER exactly, entered as a comma separated list.")
+parser.add_argument("-OS", "--owner-exact-sentence", type=str, help="(Strict) All instances where 'Owner' tag matches OWNER exactly, entered as a comma separated list.")
+#TODO : parser.add_argument("-p", "--project", type=str, help="(Loose) All instances where 'Project' tag contains PROJECT, accpets multiple values. ALWAYS DISPLAYED.")
+parser.add_argument("-PL", "--project-exact-lower", type=str, help="(Strict) All instances where 'project' tag matches PROJECT exactly, accepts multiple values.")
+parser.add_argument("-PU", "--project-exact-upper", type=str, help="(Strict) All instances where 'PROJECT' tag matches PROJECT exactly, accepts multiple values.")
+parser.add_argument("-PS", "--project-exact-sentence", type=str, help="(Strict) All instances where 'Project' tag matches PROJECT exactly, accepts multiple values.")
 parser.add_argument("-r", "--region", action='append', type=str, help="All instances in Region(s) REGION, accepts multiple values. ALWAYS DISPLAYED.")
 parser.add_argument("-R", "--region-print", action='store_true', help="Print all available region names.")
 state_args = ['pending', 'running', 'shutting-down', 'stopping', 'stopped', 'terminated']
 parser.add_argument("-s", "--state", action='append', choices=state_args, help="All instances with Instance State STATE, accepts multiple values. ALWAYS DISPLAYED.")
-parser.add_argument("-t", "--transition", help="Display last state transition details if availale.", action="store_true")
 parser.add_argument("--test", help="Debug, print all args", action="store_true")
+# Display options (value printed if argument passed)
+parser.add_argument("-l", "--launchtime", help="Display the instance launch time.", action="store_true")
+parser.add_argument("-t", "--transition", help="Display last state transition details if availale.", action="store_true")
+
 global args
 args = parser.parse_args()
 
@@ -65,6 +74,30 @@ def get_filters():
         'Values': [args.id]
         }
         filters["instance_id"] = filter_instanceid
+
+    ###################################################################    
+    # Quick and dirty - AWS API_FILTER is explicitly case-sensitive
+    #                   and do not accept logic (no OR, explicitly AND).
+    #                   Tag keys may be upper, lower, or other case.
+    #                   Case-insensitive filter should be applied
+    #                   programmatically after all results are returned
+    ###################################################################
+    # Filter for Tag : name
+    if args.name:
+        filter_name = {   # Search lowercase tag 'name'
+        'Name': str.lower('tag:name'),
+        'Values': [args.name]
+        }
+        filters["name"] = filter_name
+
+    # Filter for Tag : Name
+    if args.name:
+        filter_name = {   # Search lowercase tag 'name'
+        'Name': str.lower('tag:name'),
+        'Values': [args.name]
+        }
+        filters["name"] = filter_name
+    ###################################################################
     
     # Filter for custom tags if provided
     if args.custom_tag:
@@ -87,6 +120,7 @@ def get_filters():
 
     # Return filters
     for value in filters.values():
+#        print(value) ##DEBUG
         return value
 
 def get_region():
@@ -102,7 +136,7 @@ def get_instances():
     print("REGION\tNAME\tINSTANCE ID\tISNTANCE TYPE\tLIFECYCLE\tLAUNCH TIME\tSTATE\tLAST TRANSITION\tPRIVATE IP\tPUBLIC IP\tOWNER\tPROJECT")
     for region in arg_region:
         ec2 = boto3.resource('ec2', region)   # Print a delimiter to identify the current region
-        instances = ec2.instances.filter(   # Filter the list of returned instances
+        instances = ec2.instances.filter(   # Filter the list of returned instance - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.PlacementGroup.filters
             # List of available filters : https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeInstances.html
             Filters=[
                 get_filters()
@@ -208,7 +242,7 @@ else:
     # Go ahead and output the instance details if not checking for a list of regions
     ctags = {}
     get_instances()
-#    get_filters()
+#    print(get_filters()) ##DEBUG
 
 '''
 # Print results as a table
